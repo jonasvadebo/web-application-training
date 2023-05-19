2eee26d9824f286593d5aef424a7d469ae7201b1
Jonas Vadebo <jonas.vadebo@king.com>
2023-05-19 17:45:56 +0200

    Added http function for user controller and constructor function.

--- controller/user.go
+++ controller/user.go
@@ -1,9 +1,21 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World from the User Controller!"))
+}
+
+// Constructor function
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
 }
