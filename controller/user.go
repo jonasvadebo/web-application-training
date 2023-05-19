b9e0b98cb8c21234856e18dc0c311c270352bdf5
Jonas Vadebo <jonas.vadebo@king.com>
2023-05-19 14:55:10 +0200

    Added user controller

---
+++ controller/user.go
@@ -0,0 +1,9 @@
+package controller
+
+import "net/http"
+
+type userController struct{}
+
+func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World from the User Controller!"))
+}
